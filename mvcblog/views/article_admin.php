8850714718170ab65eb023d8a396a0d66a9bef98
Dippi12 <167083766+Dippi12@users.noreply.github.com>
2024-04-25 11:53:17 +0300

    Add files via upload

---
+++ mvcblog/views/article_admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<<meta charset="utf 8">
+<title>Концепция MVC</title>
+<link rel="stylesheet" href="…/style.css">
+<link rel="stylesheet"
+href=" https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.mi
+n.css" integrity="sha384-
+Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFCJlSAwiGgFAW/dAiS6JXm"
+crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1>Концепция MVC</h1>
+			<div>
+				<form method="post" action="index.php?action=add">
+				
+				<footer>
+				<p>Концепция MVC<br>Copyright &copy; 2018</p>
+				</footer>
+	</div>
+</body>
+</html>
